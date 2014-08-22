3538d941250d908ea58c5442880585eabebd398a
Thomas Taylor <dev@taylortom.co.uk>
2014-08-22 17:39:44 +0100

    Hook up extension to json settings

--- js/adapt-pageIncompletePrompt.js
+++ js/adapt-pageIncompletePrompt.js
@@ -6,48 +6,59 @@
 define(function(require) {
 	var Adapt = require("coreJS/adapt");
 	var PLUGIN_NAME = "_pageIncompletePrompt";
+	var model;
 
+	var isEnabled = function() {
+		var pageModel = Adapt.findById(Adapt.location._currentId);
+		var isEnabledForCourse = model && !!model._isEnabled;
+        var isEnabledForPage = pageModel.get("_pageIncompletePrompt") && !!pageModel.get("_pageIncompletePrompt")._isEnabled;        		
+		return (isEnabledForCourse && isEnabledForPage !== false) || isEnabledForPage;
+	};
+	
 	var allComponentsComplete = function() {
 		var pageModel = Adapt.findById(Adapt.location._currentId);
 		var pageComponents = pageModel.findDescendants("components").where({"_isAvailable": true});
 		var allComplete = true;
 		
 		_.each(pageComponents, function(component) {
-			var pageProgress = component.attributes._pageLevelProgress;
-			var isComplete = component.attributes._isComplete;
-			if(pageProgress && pageProgress._isEnabled && !isComplete) allComplete = false;
-		}, this);
+			var hasPageProgress = component.get("_pageLevelProgress") && component.get("_pageLevelProgress")._isEnabled;
+			var isComplete = component.get("_isComplete");
+			if(hasPageProgress && !isComplete) allComplete = false;
+		});
 		
 		return allComplete;
 	};
 	
 	var enableRouterNav = function(value) {
-		Adapt.router.set("_canNavigate", value, {pluginName: PLUGIN_NAME});
+		Adapt.router.set("_canNavigate", value,{ pluginName: PLUGIN_NAME });
 	};	
 	
-	Adapt.on("router:page", function() {
-		enableRouterNav(false);
+	/**
+	* Adapt events
+	*/
+	
+	Adapt.on("app:dataLoaded", function() {
+		model = Adapt.course.get(PLUGIN_NAME);
+	});
+	
+	Adapt.on("router:page", function(pageView) {
+		if(isEnabled()) enableRouterNav(false);
 	});
 	
 	Adapt.on("navigation:backButton", function() {
 		if(!allComponentsComplete()) {	
-			var model = Adapt.course.get(PLUGIN_NAME);
 			var promptObject = {
 				title: model.title,
 				body: model.message,
-				_prompts:[
-					{
-						promptText: model._buttons.yes,
-						_callbackEvent: "pageIncompletePrompt:leavePage",
-					},
-					{
-						promptText: model._buttons.no,
-						_callbackEvent: ""
-					}
-				],
+				_prompts:[{
+					promptText: model._buttons.yes,
+					_callbackEvent: "pageIncompletePrompt:leavePage",
+				},{
+					promptText: model._buttons.no,
+					_callbackEvent: ""
+				}],
 				_showIcon: true
 			}
-		
 			Adapt.trigger("notify:prompt", promptObject);
 		}
 		else enableRouterNav(true);
