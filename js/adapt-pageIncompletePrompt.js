0261a8030f3edbbbe1dd65d08ee0208d8fb0f5f0
Thomas Taylor <dev@taylortom.co.uk>
2014-08-22 17:11:55 +0100

    Hook up prompt data to course.json

--- js/adapt-pageIncompletePrompt.js
+++ js/adapt-pageIncompletePrompt.js
@@ -4,12 +4,12 @@
 * Maintainers - Thomas Taylor <thomas.taylor@kineo.com>
 */
 define(function(require) {
-	var Adapt = require('coreJS/adapt');
-	var PLUGIN_NAME = '_pageIncompletePrompt';
+	var Adapt = require("coreJS/adapt");
+	var PLUGIN_NAME = "_pageIncompletePrompt";
 
 	var allComponentsComplete = function() {
 		var pageModel = Adapt.findById(Adapt.location._currentId);
-		var pageComponents = pageModel.findDescendants('components').where({'_isAvailable': true});
+		var pageComponents = pageModel.findDescendants("components").where({"_isAvailable": true});
 		var allComplete = true;
 		
 		_.each(pageComponents, function(component) {
@@ -22,36 +22,39 @@ define(function(require) {
 	};
 	
 	var enableRouterNav = function(value) {
-		Adapt.router.set('_canNavigate', value, {pluginName: PLUGIN_NAME});
+		Adapt.router.set("_canNavigate", value, {pluginName: PLUGIN_NAME});
 	};	
-
-	var promptObject = {
-		title: "Leaving so soon?",
-		body: "It looks like you're trying to leave this page but you haven't completed all the learning yet. Are you sure you want to leave?",
-		_prompts:[
-		    {
-		        promptText: "Yes",
-		        _callbackEvent: "pageIncompletePrompt:leavePage",
-		    },
-		    {
-		        promptText: "No",
-		        _callbackEvent: ""
-		    }
-		],
-		_showIcon: true
-	}
-
-	Adapt.on('router:page', function() {
+	
+	Adapt.on("router:page", function() {
 		enableRouterNav(false);
 	});
 	
-	Adapt.on('navigation:backButton', function() {
-		if(!allComponentsComplete()) Adapt.trigger('notify:prompt', promptObject);
+	Adapt.on("navigation:backButton", function() {
+		if(!allComponentsComplete()) {	
+			var model = Adapt.course.get(PLUGIN_NAME);
+			var promptObject = {
+				title: model.title,
+				body: model.message,
+				_prompts:[
+					{
+						promptText: model._buttons.yes,
+						_callbackEvent: "pageIncompletePrompt:leavePage",
+					},
+					{
+						promptText: model._buttons.no,
+						_callbackEvent: ""
+					}
+				],
+				_showIcon: true
+			}
+		
+			Adapt.trigger("notify:prompt", promptObject);
+		}
 		else enableRouterNav(true);
 	});
 	
-	Adapt.on('pageIncompletePrompt:leavePage', function() {
+	Adapt.on("pageIncompletePrompt:leavePage", function() {
 		enableRouterNav(true);
-		Adapt.trigger('navigation:backButton');
+		Adapt.trigger("navigation:backButton");
 	});
 });
