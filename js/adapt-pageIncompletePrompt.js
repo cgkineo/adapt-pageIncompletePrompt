f1084bd285db13bd333e2de51090d511a712ecbb
Thomas Taylor <dev@taylortom.co.uk>
2014-08-22 13:53:34 +0100

    Refactor old code

--- js/adapt-pageIncompletePrompt.js
+++ js/adapt-pageIncompletePrompt.js
@@ -4,39 +4,26 @@
 * Maintainers - Thomas Taylor <thomas.taylor@kineo.com>
 */
 define(function(require) {
-
 	var Adapt = require('coreJS/adapt');
-	var Backbone = require('backbone');
+	var PLUGIN_NAME = '_pageIncompletePrompt';
 
 	var allComponentsComplete = function() {
-
-		if (Adapt.location._contentType !== "course") {
-			var currentPageModel = Adapt.contentObjects.findWhere({
-		        "_id": Adapt.location._currentId
-		    });
-		}
-
-		var currentPageComponents = currentPageModel.findDescendants('components').where({'_isAvailable': true});
-
-		var enabledProgressComponents = _.filter(currentPageComponents, function(component) {
-			if (component.attributes._pageLevelProgress) {
-				return component.attributes._pageLevelProgress._isEnabled;
-			}
-		});
-
-		var completedProgressComponents = _.filter(enabledProgressComponents, function(component) {
-			if (component.attributes._isComplete) {
-				return component.attributes._isComplete;
-			}
-		});
-
-		if (completedProgressComponents.length < enabledProgressComponents.length) {
-			return false;
-		} else {
-			return true;
-		}
-
+		var pageModel = Adapt.findById(Adapt.location._currentId);
+		var pageComponents = pageModel.findDescendants('components').where({'_isAvailable': true});
+		var allComplete = true;
+		
+		_.each(pageComponents, function(component) {
+			var pageProgress = component.attributes._pageLevelProgress;
+			var isComplete = component.attributes._isComplete;
+			if(pageProgress && pageProgress._isEnabled && !isComplete) allComplete = false;
+		}, this);
+		
+		return allComplete;
 	};
+	
+	var enableRouterNav = function(value) {
+		Adapt.router.set('_canNavigate', value, {pluginName: PLUGIN_NAME});
+	};	
 
 	var promptObject = {
 		title: "Leaving so soon?",
@@ -44,37 +31,27 @@ define(function(require) {
 		_prompts:[
 		    {
 		        promptText: "Yes",
-		        _callbackEvent: "pageCompletion:leavePage",
+		        _callbackEvent: "pageIncompletePrompt:leavePage",
 		    },
 		    {
 		        promptText: "No",
-		        _callbackEvent: "pageCompletion:stayOnPage"
+		        _callbackEvent: ""
 		    }
 		],
 		_showIcon: true
 	}
 
 	Adapt.on('router:page', function() {
-
-		Adapt.router.set('_canNavigate', false, {pluginName: '_pageCompletionCheck'});
-
+		enableRouterNav(false);
 	});
-
+	
 	Adapt.on('navigation:backButton', function() {
-
-		if(!allComponentsComplete()) {
-			Adapt.trigger('notify:prompt', promptObject);
-		} else {
-			Adapt.router.set('_canNavigate', true, {pluginName: '_pageIncompletePrompt'});
-		}
-
+		if(!allComponentsComplete()) Adapt.trigger('notify:prompt', promptObject);
+		else enableRouterNav(true);
 	});
-
-	Adapt.on("pageCompletion:leavePage", function() {
-
-		Adapt.router.set('_canNavigate', true, {pluginName: '_pageIncompletePrompt'});
+	
+	Adapt.on('pageIncompletePrompt:leavePage', function() {
+		enableRouterNav(true);
 		Adapt.trigger('navigation:backButton');
-
 	});
-
 });
