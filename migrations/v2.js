97f548028a71d967cd4dd3df719e18334e5c9090
Brad Simpson <brad.simpson@kineo.com>
2025-02-28 11:06:51 -0700

    Add v2.1.0 scripts

--- migrations/v2.js
+++ migrations/v2.js
@@ -1,26 +1,23 @@
 import { describe, whereFromPlugin, mutateContent, checkContent, updatePlugin, getCourse } from 'adapt-migrations';
 import _ from 'lodash';
 
-describe('Page Incomplete Prompt - v2.0.2 to v2.0.3', async () => {
-  let course, coursePIPGlobals;
-  whereFromPlugin('Page Incomplete Prompt - from v2.0.2', { name: 'adapt-pageIncompletePrompt', version: '<2.0.3' });
-  // mutateContent('Page Incomplete Prompt - add globals if missing', async (content) => {
-  //   course = getCourse();
-  //   if (!_.has(course, '_globals._extensions._pageIncompletePrompt')) _.set(course, '_globals._extensions._pageIncompletePrompt', {});
-  //   coursePIPGlobals = course._globals._extensions._pageIncompletePrompt;
-  //   return true;
-  // });
-  // mutateContent('Page Incomplete Prompt - add globals _navOrder', async (content) => {
-  //   _.set(coursePIPGlobals, '_navOrder', 1);
-  //   return true;
-  // });
-  // checkContent('Page Incomplete Prompt - check globals _navOrder attribute', async content => {
-  //   if (coursePIPGlobals === undefined) throw new Error('Page Incomplete Prompt - globals _pageIncompletePrompt invalid');
-  //   return true;
-  // });
-  // checkContent('Page Incomplete Prompt - check globals _navOrder attribute', async content => {
-  //   if (coursePIPGlobals._navOrder !== 1) throw new Error('Page Incomplete Prompt - globals _navOrder invalid');
-  //   return true;
-  // });
-  updatePlugin('Page Incomplete Prompt - update to v2.0.3', { name: 'adapt-pageIncompletePrompt', version: '2.0.3', framework: '^2.0.0' });
+describe('Page Incomplete Prompt - v2.0.8 to v2.1.0', async () => {
+  let course, coursePIP;
+  whereFromPlugin('Page Incomplete Prompt - from v2.0.8', { name: 'adapt-pageIncompletePrompt', version: '<2.1.0' });
+  mutateContent('Page Incomplete Prompt - add course _pageIncompletePrompt._classes', async (content) => {
+    course = getCourse();
+    if (!_.has(course, '_pageIncompletePrompt')) _.set(course, '_pageIncompletePrompt', {});
+    coursePIP = course._pageIncompletePrompt;
+    coursePIP._classes = '';
+    return true;
+  });
+  checkContent('Page Incomplete Prompt - check course _pageIncompletePrompt object', async content => {
+    if (coursePIP === undefined) throw new Error('Page Incomplete Prompt - course _pageIncompletePrompt invalid');
+    return true;
+  });
+  checkContent('Page Incomplete Prompt - check course _pageIncompletePrompt._isHidden', async content => {
+    if (coursePIP._classes === undefined) throw new Error('Page Incomplete Prompt - course _pageIncompletePrompt._classes invalid');
+    return true;
+  });
+  updatePlugin('Page Incomplete Prompt - update to v2.1.0', { name: 'adapt-pageIncompletePrompt', version: '2.1.0', framework: '>=3.3' });
 });
