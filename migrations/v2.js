d4b595382c3650c780bed75b2540db6729aea575
Brad Simpson <brad.simpson@kineo.com>
2025-02-28 11:33:35 -0700

    V2

--- migrations/v2.js
+++ migrations/v2.js
@@ -1,18 +1,12 @@
 import { describe, whereFromPlugin, mutateContent, checkContent, updatePlugin, getCourse } from 'adapt-migrations';
-import _ from 'lodash';
 
 describe('Page Incomplete Prompt - v2.0.8 to v2.1.0', async () => {
   let course, coursePIP;
   whereFromPlugin('Page Incomplete Prompt - from v2.0.8', { name: 'adapt-pageIncompletePrompt', version: '<2.1.0' });
   mutateContent('Page Incomplete Prompt - add course _pageIncompletePrompt._classes', async (content) => {
     course = getCourse();
-    if (!_.has(course, '_pageIncompletePrompt')) _.set(course, '_pageIncompletePrompt', {});
     coursePIP = course._pageIncompletePrompt;
     coursePIP._classes = '';
-    return true;
-  });
-  checkContent('Page Incomplete Prompt - check course _pageIncompletePrompt object', async content => {
-    if (coursePIP === undefined) throw new Error('Page Incomplete Prompt - course _pageIncompletePrompt invalid');
     return true;
   });
   checkContent('Page Incomplete Prompt - check course _pageIncompletePrompt._isHidden', async content => {
